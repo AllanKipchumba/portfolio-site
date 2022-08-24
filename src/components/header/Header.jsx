3d4ab9e1d3aa216b4b917e371e3ed9fb317ce107
AllanKipchumba <allankipchumba76@gmail.com>
2022-08-24 11:21:30 +0300

    updated header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,21 +7,28 @@ export const Header = () => {
   return (
     <div className="header">
       <Navbar />
-      <div className="text-white pt-[30%] pl-[30%]">
-        <h1>welcome to my website</h1>
+      <div className="text-white pt-[30%]">
+        <h1>Hello!</h1>
+        <h1 className="text-center uppercase tracking-[6px] font-bold text-lg">
+          welcome to my website
+        </h1>
 
-        <p>
+        <p className="mt-8 ml-[200px] ">
           {" "}
           I'm {""}
           <Typed
-            strings={["Allan", "A web developer", "react"]}
+            strings={["Allan kipchumba", "A web designer", "A web developer"]}
             typeSpeed={150}
             backSpeed={100}
             loop
             showCursor={true}
-            className="text-xl sm:text-4xl md:text-5xl text-[#00df9a] font-bold uppercase md:pl-4 pl-2"
+            className="text-xl sm:text-4xl md:text-4xl font-bold uppercase md:pl-4 pl-2 text-[#ffbd39]"
           />
         </p>
+      </div>
+      <div className="btn mt-20 flex gap-6 ml-[260px]">
+        <button>View Work</button>
+        <button>Hire Me</button>
       </div>
     </div>
   );
