7c95e344fbba3520128a2fa71ff022d4dd497dce
AllanKipchumba <allankipchumba76@gmail.com>
2022-08-28 21:35:32 +0300

    populated footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { About } from "./components/about/About";
 import { Education } from "./components/education/Education";
+import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
 import { Projects } from "./components/projects/Projects";
 import Skills from "./components/skills/Skills";
@@ -13,6 +14,7 @@ export const App = () => {
       <Education />
       <Skills />
       <Projects />
+      <Footer />
     </>
   );
 };
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./footer.scss";
+import {
+  AiOutlineTwitter,
+  AiOutlineGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+import { BsFacebook, BsFillTelephoneFill } from "react-icons/bs";
+import { SiMinutemailer } from "react-icons/si";
+
+export const Footer = () => {
+  return (
+    <>
+      <div className="footer py-14">
+        <div className="max-w-[80%] mx-auto">
+          <div className="flex gap-5 justify-center">
+            <a href="#">
+              <AiOutlineGithub className="icon" />
+            </a>
+            <a href="#">
+              <AiOutlineTwitter className="icon" />
+            </a>
+            <a href="#">
+              <AiOutlineInstagram className="icon" />
+            </a>
+            <a href="#">
+              <BsFacebook className="icon" />
+            </a>
+          </div>
+          <div className="flex  gap-8 justify-center mt-8">
+            <div>
+              <BsFillTelephoneFill />
+              <p className="text-[#808080]">+2547 22 419 582</p>
+            </div>
+            <div>
+              <SiMinutemailer />
+              <p className="text-[#808080]">allankipchumba76@gmail.com</p>
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <p className="text-[#E6E6E6]">
+              Copyright &copy; {new Date().getFullYear()} | AllanKipchumba
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
