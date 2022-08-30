cf374d255955e1fb4e574ed36cbebb27fdf5a1bf
AllanKipchumba <allankipchumba76@gmail.com>
2022-08-30 19:34:28 +0300

    redesigned projects an footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,30 +13,58 @@ export const Footer = () => {
     <>
       <div className="footer py-14" id="footer">
         <div className="max-w-[80%] mx-auto">
-          <div className="flex gap-5 justify-center">
-            <a href="#">
-              <AiOutlineGithub className="icon" />
-            </a>
-            <a href="#">
-              <AiOutlineTwitter className="icon" />
-            </a>
-            <a href="#">
-              <AiOutlineInstagram className="icon" />
-            </a>
-            <a href="#">
-              <BsFacebook className="icon" />
-            </a>
-          </div>
-          <div className="flex  gap-8 justify-center mt-8">
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-12">
             <div>
-              <BsFillTelephoneFill />
-              <p className="text-[#808080]">+2547 22 419 582</p>
+              <h1>About</h1>
+              <p>
+                This is the official website for Allan Kipchumba. Allan is a
+                passionate developer who is deeply invested in software
+                engineering and loves everything about coding. coding is
+                intriguing as it shappens one's wits, creative thinking and
+                strategic thinking skills. you pay more attention to detail in
+                your environment.
+              </p>
+              <h2>You can find me on</h2>
+              <div className="flex">
+                <a href="#">
+                  <AiOutlineGithub className="icon" />
+                </a>
+                <a href="#">
+                  <AiOutlineTwitter className="icon" />
+                </a>
+                <a href="#">
+                  <AiOutlineInstagram className="icon" />
+                </a>
+                <a href="#">
+                  <BsFacebook className="icon" />
+                </a>
+              </div>
             </div>
             <div>
-              <SiMinutemailer />
-              <p className="text-[#808080]">allankipchumba76@gmail.com</p>
+              <h1>Quick links</h1>
+              <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>skills</li>
+                <li>projects</li>
+                <li>contact</li>
+              </ul>
+            </div>
+            <div>
+              <h1>Have an inquiry?</h1>
+              <div>
+                <div>
+                  <BsFillTelephoneFill />
+                  <p className="text-[#808080]">+2547 22 419 582</p>
+                </div>
+                <div>
+                  <SiMinutemailer />
+                  <p className="text-[#808080]">allankipchumba76@gmail.com</p>
+                </div>
+              </div>
             </div>
           </div>
+
           <div className="flex justify-center mt-8">
             <p className="text-[#E6E6E6]">
               Copyright &copy; {new Date().getFullYear()} | AllanKipchumba
@@ -47,3 +75,6 @@ export const Footer = () => {
     </>
   );
 };
+
+// 1. Refactored menu items to one js file, menuItems.js 2. removed MenuButtons
+// component. the comonentwas rendering a button element which can be directly rende red in the Menu.jsx file. 3. Added key to button in Menu.jsx 4. update state to index of buton clickedt
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import "./projects.scss";
-import sogy from "./assets/sogy.JPG";
-import simon from "./assets/simon.JPG";
-import { StoryHub } from "./portfolio/StoryHub";
-import { TaskmanagerApi } from "./portfolio/TaskmanagerApi";
 
 export const Projects = () => {
   return (
@@ -14,53 +10,21 @@ export const Projects = () => {
         </h1>
         <div className="container grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-[80%] mx-auto pb-[100px]">
           <div>
-            <StoryHub />
+            <h1>StoryHub</h1>
+            <button>read more</button>
           </div>
-          <TaskmanagerApi />
-          <div></div>
+
           <div>
-            <img src={sogy} alt="landing-page" />
+            <h1>Task Manager-API</h1>
+            <button>read more</button>
+          </div>
+          <div>
             <h1>Landing Page</h1>
-            <p>
-              This was my first GitHub react project. It is a straightforward
-              landing page on a hotel website that provides prospective
-              customers with a tour of the hotel as well as crucial links.
-            </p>
-            <p>Technologies: React, CSS3</p>
-            <div>
-              <button>
-                <a href="https://sogy-hotel.netlify.app/#/">View Demo</a>
-              </button>
-              <button>
-                <a href="https://github.com/AllanKipchumba/Sogy-Hotel">
-                  view code
-                </a>
-              </button>
-            </div>
+            <button>read more</button>
           </div>
           <div>
-            <img src={simon} alt="simon-game" />
-            <h1>Simon Game Clone</h1>
-            <p>
-              Simon is an electronic game of short-term memory skill invented by
-              Ralph H. Baer and Howard J. Morrison. The device produces a series
-              of tones and lights that the user must repeat. If the user is
-              successful, the sequence lengthens and becomes more complicated.
-              When the user fails or the time limit expires, the game is ended.
-            </p>
-            <p>Technologies: Javascript, HTML5, CSS3</p>
-            <div>
-              <button>
-                <a href="https://allankipchumba.github.io/simon_game_clone/">
-                  View Demo
-                </a>
-              </button>
-              <button>
-                <a href="https://github.com/AllanKipchumba/simon_game_clone">
-                  view code
-                </a>
-              </button>
-            </div>
+            <h1>SimonGame-Clone</h1>
+            <button>read more</button>
           </div>
         </div>
       </div>
