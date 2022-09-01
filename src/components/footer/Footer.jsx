1cdded7412679d6aba4fd8570a1b2c495b9bff50
AllanKipchumba <allankipchumba76@gmail.com>
2022-09-01 18:40:13 +0300

    updated styling and links in footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,73 +5,117 @@ import {
   AiOutlineGithub,
   AiOutlineInstagram,
 } from "react-icons/ai";
-import { BsFacebook, BsFillTelephoneFill } from "react-icons/bs";
+import { BsFillTelephoneFill } from "react-icons/bs";
 import { SiMinutemailer } from "react-icons/si";
+import { Link, animateScroll as scroll } from "react-scroll";
 
 export const Footer = () => {
   return (
-    <>
-      <div className="footer py-14" id="footer">
-        <div className="max-w-[80%] mx-auto">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-12">
-            <div>
-              <h1>About</h1>
-              <p>
-                This is the official website for Allan Kipchumba. Allan is a
-                passionate developer who is deeply invested in software
-                engineering and loves everything about coding. coding is
-                intriguing as it shappens one's wits, creative thinking and
-                strategic thinking skills. you pay more attention to detail in
-                your environment.
-              </p>
-              <h2>You can find me on</h2>
-              <div className="flex">
-                <a href="#">
-                  <AiOutlineGithub className="icon" />
-                </a>
-                <a href="#">
-                  <AiOutlineTwitter className="icon" />
-                </a>
-                <a href="#">
-                  <AiOutlineInstagram className="icon" />
-                </a>
-                <a href="#">
-                  <BsFacebook className="icon" />
-                </a>
-              </div>
-            </div>
-            <div>
-              <h1>Quick links</h1>
-              <ul>
-                <li>home</li>
-                <li>about</li>
-                <li>skills</li>
-                <li>projects</li>
-                <li>contact</li>
-              </ul>
-            </div>
-            <div>
-              <h1>Have an inquiry?</h1>
-              <div>
-                <div>
-                  <BsFillTelephoneFill />
-                  <p className="text-[#808080]">+2547 22 419 582</p>
-                </div>
-                <div>
-                  <SiMinutemailer />
-                  <p className="text-[#808080]">allankipchumba76@gmail.com</p>
-                </div>
-              </div>
+    <div className="footer py-14" id="footer">
+      <div className="max-w-[80%] mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-6 md:gap-12">
+          <div className="md:col-span-2">
+            <h1>About</h1>
+            <p className="text-[#808080]">
+              This is the official website for Allan Kipchumba. Allan is a
+              passionate developer who is deeply invested in software
+              engineering and loves everything about coding. coding is
+              intriguing as it shappens one's wits, creative thinking and
+              strategic thinking skills. you pay more attention to detail in
+              your environment.
+            </p>
+            <h2 className="text-[#808080] mt-3">You can find me on</h2>
+
+            <div className="flex gap-2 mt-3">
+              <a href="https://github.com/AllanKipchumba" target="self">
+                <AiOutlineGithub className="icon" />
+              </a>
+              <a href="https://twitter.com/devAllan_" target="self">
+                <AiOutlineTwitter className="icon" />
+              </a>
+              <a
+                href="https://www.instagram.com/allan_kipchumba/"
+                target="self"
+              >
+                <AiOutlineInstagram className="icon" />
+              </a>
             </div>
           </div>
 
-          <div className="flex justify-center mt-8">
-            <p className="text-[#E6E6E6]">
-              Copyright &copy; {new Date().getFullYear()} | AllanKipchumba
-            </p>
+          <div>
+            <h1>Quick links</h1>
+            <ul>
+              <li
+                onClick={() => {
+                  scroll.scrollToTop();
+                }}
+              >
+                home
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-65}
+                  duration={1000}
+                >
+                  about
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="skills"
+                  spy={true}
+                  smooth={true}
+                  offset={-65}
+                  duration={1000}
+                >
+                  Skills
+                </Link>
+              </li>
+              <li>
+                <Link
+                  activeClass="active"
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={-65}
+                  duration={1000}
+                >
+                  projects
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="md:col-span-2">
+            <h1>Have an inquiry?</h1>
+            <div>
+              <div className="flex gap-2 mb-6">
+                <BsFillTelephoneFill />
+                <p className="text-[#808080]">+2547 22 419 582</p>
+              </div>
+              <div className="flex gap-2">
+                <SiMinutemailer />
+                <a
+                  href="mailto:allankipchumba@gmail.com"
+                  className="text-[#808080]"
+                >
+                  allankipchumba76@gmail.com
+                </a>
+              </div>
+            </div>
           </div>
         </div>
+
+        <div className="flex justify-center mt-8">
+          <p className="text-[#E6E6E6]">
+            Copyright &copy; {new Date().getFullYear()} | AllanKipchumba
+          </p>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
