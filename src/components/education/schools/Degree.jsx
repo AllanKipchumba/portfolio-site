c3b693458f57c65adb78ac64c1bff2042cbbbcfa
AllanKipchumba <allankipchumba76@gmail.com>
2022-09-03 07:56:49 +0300

    updated content on degree component

--- src/components/education/schools/Degree.jsx
+++ src/components/education/schools/Degree.jsx
@@ -6,7 +6,7 @@ export const Degree = () => {
   return (
     <>
       <h1>2018 - present</h1>
-      <h2>Bachelor's Degree of Engineering</h2>
+      <h2>Bachelor of Engineering</h2>
       <h3>moi university</h3>
       <button onClick={() => setHide(!hide)}>
         {hide ? `Close` : `Read More`}
@@ -14,12 +14,20 @@ export const Degree = () => {
 
       {hide && (
         <p>
-          I am an electrical and electronics engineering student. I'm in my
-          fourth year(2022) of college. Everything about the engineering
-          curriculum appeals to me because it encourages students to think
-          outside the box. The development of logical thinking, objectivity,
-          critical thinking, and creativity has greatly influenced how I see the
-          world.
+          I am a fourth-year Electrical and Electronics Engineering student at
+          Moi University. Problem solving is central to the engineering
+          curriculum. The curriculum fundamentally covers a wide range of
+          subjects, including mechanics, telecommunications, electronics, power,
+          computer engineering, advanced mathematics, and applied physics. This
+          instills a great deal of creativity, critical thinking, logical
+          thinking, objectivity, and discipline. Furthermore, the curriculum
+          teaches you to think outside the box, pay attention to details, and
+          develop a broad range of technical thinking skills. When approaching a
+          problem, you are constantly challenged to stretch your thinking and
+          consider all possibilities. It is an intriguing STEM field that I
+          would recommend it to anyone who is interested in Science, Technology,
+          Engineering and Mathematics. Every experience I've had in engineering
+          school is immensely valuable to me.
         </p>
       )}
     </>
